e835a4ea3c63e8c2adbdf7f596c279dee63b50b3
jusoaresg <julianosgreg@gmail.com>
2025-03-18 14:07:11 -0300

    fix: texto Meus Estudos feat: adição icone conhecimento no topbar de meus estudos

--- app/src/main/java/br/com/fiap/fineduca/MainActivity.kt
+++ app/src/main/java/br/com/fiap/fineduca/MainActivity.kt
@@ -3,19 +3,27 @@ package br.com.fiap.fineduca
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -55,7 +63,14 @@ class MainActivity : ComponentActivity() {
                         
                     composable("estudos") {
                         BaseScreen(modifier = baseScreenModifier, topBar = {
-                            Text(text = "Meus Estudo", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Text(text = "Meus Estudos", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+                                Image(modifier = Modifier
+                                    .size(48.dp)
+                                    .offset(x = 4.dp, y = -4.dp),
+                                    painter = painterResource(id = R.drawable.conhecimento),
+                                    contentDescription = "conhecimento")
+                            }
                         }, bottomBar = { voltarButton() }) {
                             EstudosScreen(modifier = Modifier, navController = navController)
                         }
